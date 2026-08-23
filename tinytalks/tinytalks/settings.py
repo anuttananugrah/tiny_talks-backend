f71d7fe35f907787a177fd407445dd97cd68b0d7
ANUGRAH <anuttananugrah@gmail.com>
2026-08-24 00:18:15 +0530

    adding bravo api

--- tinytalks/tinytalks/settings.py
+++ tinytalks/tinytalks/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions", "django.contrib.messages", "django.contrib.staticfiles",
     "rest_framework", "rest_framework_simplejwt", "rest_framework_simplejwt.token_blacklist",
     "corsheaders", "cloudinary", "cloudinary_storage", "live_session", "user",
-    "listening_session", "reading_session",
+    "listening_session", "reading_session","anymail",
 ]
 
 MIDDLEWARE = [
@@ -96,15 +96,16 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
-SERVER_EMAIL = config("SERVER_EMAIL", default=EMAIL_HOST_USER)
+# Anymail Brevo Configuration
+EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+
+ANYMAIL = {
+    "BREVO_API_KEY": config("BREVO_API_KEY", default=""),
+}
+
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="tinytalksbysona@gmail.com")
+SERVER_EMAIL = config("SERVER_EMAIL", default="tinytalksbysona@gmail.com")
+
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"), "API_KEY": config("CLOUDINARY_API_KEY"),
     "API_SECRET": config("CLOUDINARY_API_SECRET"),
